678e6220b6f8fd58cbf80b758164508b5e6b11cb
webmsgr <zapcontact000@gmail.com>
2020-11-17 15:26:00 -0800

    make skipper only load once

--- skipper.js
+++ skipper.js
@@ -1,29 +1,31 @@
 window.API = document.querySelector("#stageFrame").contentWindow.API
 window.Player = document.querySelector("#stageFrame").contentWindow.Player
-
-function skipper_next() {
-    if (API.Frame.isComplete()) {
-        API.FrameChain.nextFrame()
+if (typeof window.skipper == "undefined") {
+    window.skipper = "YES"
+    function skipper_next() {
+        if (API.Frame.isComplete()) {
+            API.FrameChain.nextFrame()
+        }
     }
-}
 
-function skipper_videoDone(target, thisarg, argumentslist) {
-    if (argumentslist.length == 0) {
-        target()
-    } else {
-        target(argumentslist[0])
+    function skipper_videoDone(target, thisarg, argumentslist) {
+        if (argumentslist.length == 0) {
+            target()
+        } else {
+            target(argumentslist[0])
+        }
+        skipper_next()
     }
-    skipper_next()
-}
 
-function skipper_init() {
-    API.Frame.complete = new Proxy(API.Frame.complete, {
-        apply: skipper_videoDone
-    });
-    API.Frame.highlightNextFrameButton = new Proxy(API.Frame.highlightNextFrameButton, {
-        apply: skipper_videoDone
-    });
-    console.log("edgenuity-skipper by Wackery is now active.")
-}
+    function skipper_init() {
+        API.Frame.complete = new Proxy(API.Frame.complete, {
+            apply: skipper_videoDone
+        });
+        API.Frame.highlightNextFrameButton = new Proxy(API.Frame.highlightNextFrameButton, {
+            apply: skipper_videoDone
+        });
+        console.log("edgenuity-skipper by Wackery is now active.")
+    }
 
-skipper_init();
+    skipper_init();
+}
