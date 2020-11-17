c3282c9ecaf2c184c29c3610a88794ba5cc3493e
webmsgr <zapcontact000@gmail.com>
2020-11-17 14:00:15 -0800

    code

--- skipper.js
+++ skipper.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+window.API = document.querySelector("#stageFrame").contentWindow.API
+function skipper_next() {
+    if (API.Frame.isComplete()) {
+        API.FrameChain.nextFrame()
+    }
+}
+function skipper_last() {
+    while (API.Frame.isComplete()) {
+        skipper_next()
+    }
+}
+function skipper_init() {
+
+}
+
+
+
+skipper_init();
