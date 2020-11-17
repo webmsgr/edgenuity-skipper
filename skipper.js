4d18b82e237a9d4997aabb51366924a7aaae12c4
webmsgr <zapcontact000@gmail.com>
2020-11-17 15:02:51 -0800

    aya

--- skipper.js
+++ skipper.js
@@ -17,12 +17,12 @@ function skipper_questionloadoverride(target,thisarg,argumentslist) {
     target()
 }
 function skipper_videoDone(target,thisarg,argumentslist) {
-    target()
+    target(argumentslist[0])
     skipper_next()
 }
 function skipper_init() {
     API.Frame.loadQuestions = new Proxy(API.Frame.loadQuestions, {apply: skipper_questionloadoverride });
-    API.Video.videoDone = new Proxy(API.Video.videoDone, {apply: skipper_questionloadoverride });
+    API.Frame.complete = new Proxy(API.Frame.complete, {apply: skipper_videoDone });
     skipper_last()
 
 }
