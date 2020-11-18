2012e7d5d2d65976bb4e19600fad477fa27ddd51
webmsgr <zapcontact000@gmail.com>
2020-11-18 15:19:23 -0800

    code or i will end your entire existance

--- minify.bat
+++ minify.bat
@@ -1,3 +1,6 @@
 @echo off
-uglifyjs src/skipper.js --compress --mangle -o release/skipper.min.js --source-map "url='https://webmsgr.github.io/edgenuity-skipper/release/skipper.min.js.map'"
+cd release
+uglifyjs ../src/skipper.js --compress --mangle -o skipper.min.js --source-map "url='https://webmsgr.github.io/edgenuity-skipper/release/skipper.min.js.map'"
 '"
+cd ..
+pause
