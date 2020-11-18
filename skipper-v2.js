2b42fb44eee78578d4fe3aa2e8bf313ec755dfc3
webmsgr <zapcontact000@gmail.com>
2020-11-18 14:49:27 -0800

    use debounce func

--- skipper-v2.js
+++ skipper-v2.js
@@ -1,22 +1,27 @@
 window.API = document.querySelector("#stageFrame").contentWindow.API
-window.cooldown = false
-window.cooldownTimeout = 0
 API.autoplay = true
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function executedFunction() {
+        var context = this;
+        var args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 API.Video.videoDone = new Proxy(API.Video.videoDone, {
-    apply: function (target, thisarg, argumentslist) {
+    apply: debounce(function (target, thisarg, argumentslist) {
         target(argumentslist[0])
-        if (!cooldown) {
-            if (API.autoplay) {
-                setTimeout(API.FrameChain.nextFrame, 100)
-                cooldown = true
-                cooldownTimeout = setTimeout(function () {
-                    cooldown = false
-                }, 500)
-            }
-        } else {
-            clearTimeout(cooldownTimeout)
-            cooldown = false
+        if (API.autoplay) {
+            setTimeout(API.FrameChain.nextFrame, 100)
         }
-    }
+    }, 500)
 });
+
 console.log("edgenuity-skipper now active. Version 2")
