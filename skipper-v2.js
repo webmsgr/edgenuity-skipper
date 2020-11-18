b4ab564c0c68f882ca81fda973247f4458577287
webmsgr <zapcontact000@gmail.com>
2020-11-18 12:30:44 -0800

    add framework for enabling/disabling autoplay

--- skipper-v2.js
+++ skipper-v2.js
@@ -1,15 +1,18 @@
 window.API = document.querySelector("#stageFrame").contentWindow.API
 window.cooldown = false
 window.cooldownTimeout = 0
+API.autoplay = true
 API.Frame.complete = new Proxy(API.Frame.complete, {
     apply: function (target, thisarg, argumentslist) {
         target(argumentslist[0])
         if (!cooldown) {
-            setTimeout(API.FrameChain.nextFrame,100)
-            cooldown = true
-            cooldownTimeout = setTimeout(function () {
-                cooldown = false
-            },500)
+            if (API.autoplay) {
+                setTimeout(API.FrameChain.nextFrame, 100)
+                cooldown = true
+                cooldownTimeout = setTimeout(function () {
+                    cooldown = false
+                }, 500)
+            }
         } else {
             clearTimeout(cooldownTimeout)
             cooldown = false
@@ -17,4 +20,3 @@ API.Frame.complete = new Proxy(API.Frame.complete, {
     }
 });
 console.log("edgenuity-skipper now active. Version 2")
-
