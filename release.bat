1b6a09d754f4a8e188347d20b6e2edef93a4e40e
webmsgr <zapcontact000@gmail.com>
2020-11-18 15:31:32 -0800

    Release

--- release.bat
+++ release.bat
@@ -1,7 +1,7 @@
 @echo off
 cd release
-uglifyjs ../src/skipper.js --compress --mangle -o skipper.min.js --source-map "url='https://webmsgr.github.io/edgenuity-skipper/release/skipper.min.js.map'"
-'"
+call cmd /c uglifyjs ../src/skipper.js --compress --mangle -o skipper.min.js --source-map "url=https://webmsgr.github.io/edgenuity-skipper/release/skipper.min.js.map"
+
 cd ..
 git add release/
 git commit
